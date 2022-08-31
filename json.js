bf10bfac168ae066de10eda4fc4a70dfe476c08b
chithiraivel <chithiraivels11@gmail.com>
2022-08-31 23:34:35 +0530

    json.

---
+++ json.js
@@ -0,0 +1,86 @@
+
+// var person={
+
+//     firstName : "chithirai",
+//     lastName  : "vel",
+//     age  : 24,
+
+//     detail:function(){
+//         return(this.firstName + " " + this.lastName);
+//     }
+// }
+
+// document.write(person.detail());
+
+
+
+let mobiles=[
+    {
+        "Name" : "Samsung Galaxy M32",
+        "brand" : "Samsung",
+        "mrp" : 15000.00,
+        "price" : 14000.00,
+        "image" :"https://images-eu.ssl-images-amazon.com/images/I/41ZGJxnJu1S._SX300_SY300_QL70_FMwebp_.jpg",
+        "Storage" : "64GB",
+        "ram" : "4GB",
+        "camera" : "48MP"
+    },
+
+    {
+        "Name" : "Samsung Galaxy M31",
+        "brand" : "Samsung",
+        "mrp" : 15000.00,
+        "price" : 14000.00,
+        "image" :"https://images-eu.ssl-images-amazon.com/images/I/41ZGJxnJu1S._SX300_SY300_QL70_FMwebp_.jpg",
+        "Storage" : "64GB",
+        "ram" : "4GB",
+        "camera" : "48MP"
+    },
+
+    {
+        "Name" : "Samsung Galaxy M30",
+        "brand" : "Samsung",
+        "mrp" : 15000.00,
+        "price" : 14000.00,
+        "image" :"https://images-eu.ssl-images-amazon.com/images/I/41ZGJxnJu1S._SX300_SY300_QL70_FMwebp_.jpg",
+        "Storage" : "64GB",
+        "ram" : "4GB",
+        "camera" : "48MP"
+    }
+]
+console.log(mobiles);
+
+
+
+
+// string to convert javascript object
+
+//var str = '{"name" : "vel", "age" :24}';
+
+// alert(typeof(str));
+
+//var obj=JSON.parse(str);
+
+// alert(typeof(obj));
+
+//document.write(obj.name);
+
+
+
+// Javascript object to JSON object
+
+// var obj={
+
+//     name:"thunder",
+//     age:1500,
+// };
+// alert(typeof(obj));
+
+//var str=JSON.stringify(obj);
+//document.write(str1.name)
+// alert(typeof(str));
+
+
+
+
+
